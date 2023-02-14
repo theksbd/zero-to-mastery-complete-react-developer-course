43b412fde0ffca19d3ade1774aad9a2f2dccc93c
Do Thien Hoang <theksbd@gmail.com>
2023-02-14 22:04:40 +0700

    first commit of fashionista

---
+++ crwn-clothing/src/components/Directory/Directory.jsx
@@ -0,0 +1,14 @@
+import './Directory.scss';
+import CategoryItem from '../CategoryItem/CategoryItem';
+
+const Directory = ({ categories }) => {
+  return (
+    <div className='directory-container'>
+      {categories.map(category => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
