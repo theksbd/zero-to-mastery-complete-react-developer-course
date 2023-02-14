33cd1e30463db64b972420e114c70f38fe0a1c34
Do Thien Hoang <theksbd@gmail.com>
2023-02-14 15:06:04 +0700

    update monsters rolodex using Functional Component

--- monsters-rolodex-functional-component/src/App.jsx
+++ monsters-rolodex-functional-component/src/App.jsx
@@ -40,48 +40,4 @@ const App = () => {
   );
 };
 
-// class App extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchTerm: ''
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch('https://jsonplaceholder.typicode.com/users')
-//       .then(response => response.json())
-//       .then(data => this.setState({ monsters: data }))
-//       .catch(err => console.log(err));
-//   }
-
-//   handleSearchChange = e => {
-//     const searchTerm = e.target.value.toLowerCase();
-//     this.setState({ searchTerm });
-//   };
-
-//   render() {
-//     const { monsters, searchTerm } = this.state;
-//     const { handleSearchChange } = this;
-
-//     const filteredMonsters = monsters.filter(monster =>
-//       monster.name.toLowerCase().includes(searchTerm)
-//     );
-
-//     return (
-//       <div>
-//         <h1 className='app-title'>Monster Rolodex</h1>
-//         <SearchBar
-//           className='search-box'
-//           placeholder='Search Monsters'
-//           onChange={handleSearchChange}
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
-
 export default App;
