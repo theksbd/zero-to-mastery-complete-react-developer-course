2e2e43ee40fb5475ef0d3ca3e5436d51a6afd778
Do Thien Hoang <theksbd@gmail.com>
2023-02-20 18:07:57 +0700

    fix bug after deploying

--- crwn-clothing/vite.config.js
+++ crwn-clothing/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), viteCommonjs()],
   server: {
     port: 3000
   }
